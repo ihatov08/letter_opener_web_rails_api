648f953b76d7b335f100309ad21e08d1283626d5
ihatov08 <ihatov08@gmail.com>
2021-10-28 23:33:22 +0900

    add sample mailer

---
+++ app/mailers/sample_mailer.rb
@@ -0,0 +1,13 @@
+class SampleMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.sample_mailer.hello.subject
+  #
+  def hello
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
